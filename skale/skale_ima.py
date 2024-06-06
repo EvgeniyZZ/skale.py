04888cc83f6eff4cae77783f440028d3e30a3b59
Evgeniy <evgeniy@skalelabs.com>
2024-06-06 22:29:47 +0100

    add contracts info

--- skale/skale_ima.py
+++ skale/skale_ima.py
@@ -32,18 +32,47 @@ logger = logging.getLogger(__name__)
 
 CONTRACTS_INFO = [
     ContractInfo('linker', 'Linker',
-                 contracts.Linker, ContractTypes.API, False)
+                 contracts.Linker, ContractTypes.API, False),
+    ContractInfo('deposit_box_eth', 'DepositBoxEth',
+                 contracts.DepositBoxEth, ContractTypes.API, False),
+    ContractInfo('deposit_box_erc20', 'DepositBoxERC20',
+                 contracts.DepositBoxERC20, ContractTypes.API, False),
+    ContractInfo('deposit_box_erc721', 'DepositBoxERC721',
+                 contracts.DepositBoxERC721, ContractTypes.API, False),
+    ContractInfo('community_pool', 'CommunityPool',
+                 contracts.CommunityPool, ContractTypes.API, False),
+    ContractInfo('community_locker', 'CommunityLocker',
+                 contracts.CommunityLocker, ContractTypes.API, False),
+    ContractInfo('token_manager_erc20', 'TokenManagerERC20',
+                 contracts.TokenManagerERC20, ContractTypes.API, False),
+    ContractInfo('token_manager_erc721', 'TokenManagerERC721',
+                 contracts.TokenManagerERC721, ContractTypes.API, False),
+    ContractInfo('token_manager_erc721_wmt', 'TokenManagerERC721WithMetadata',
+                 contracts.TokenManagerERC721WithMetadata, ContractTypes.API, False),
+    ContractInfo('token_manager_erc1155', 'TokenManagerERC1155',
+                 contracts.TokenManagerERC1155, ContractTypes.API, False),
+    ContractInfo('token_manager_eth', 'TokenManagerETH',
+                 contracts.TokenManagerETH, ContractTypes.API, False),
+    ContractInfo('message_proxy_for_mainnet', 'MessageProxyForMainnet',
+                 contracts.MessageProxyForMainnet, ContractTypes.API, False)
 ]
+
+    def contracts_info(self) -> List[ContractInfo[SkaleIma]]:
+        import skale.contracts.ima as contracts
+        return [
+            ContractInfo('linker', 'Linker',
+                         contracts.Linker, ContractTypes.API, False)
+        ]
 
     def set_contracts_info(self) -> None:
         self._SkaleBase__contracts_info = get_contracts_info(self.contracts_info())
 
+class SkaleIma(SkaleBase): 
+    @property
+    def project_name(self) -> str:
+        return 'schain-ima'  #mainnet-ima
+
 
 def spawn_skale_ima_lib(skale_ima: SkaleIma) -> SkaleIma:
     """ Clone skale ima object with the same wallet """
-    return SkaleIma(skale_ima._endpoint, skale_ima._abi_filepath, skale_ima.wallet)
-
-
-class SkaleIma(SkaleBase):
-    def set_contracts_info(self):
-        self._SkaleBase__contracts_info = get_contracts_info(CONTRACTS_INFO)
+    return SkaleIma(skale_ima._endpoint, skale_ima.instance.address, skale_ima.wallet)
