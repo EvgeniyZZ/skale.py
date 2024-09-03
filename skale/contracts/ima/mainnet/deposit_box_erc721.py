c562f19cbc303fc8b8babadd6aca956f053463e0
EvgeniyZh <52660712+EvgeniyZZ@users.noreply.github.com>
2024-09-03 18:17:54 +0100

    Update deposit_box_erc721.py

--- skale/contracts/ima/mainnet/deposit_box_erc721.py
+++ skale/contracts/ima/mainnet/deposit_box_erc721.py
@@ -11,7 +11,8 @@ class DepositBoxERC721(BaseContract):
     @transaction_method
     def add_erc721_token(self, schain_name: str, address: int) -> TxRes:
         return self.contract.functions.addERC721TokenByOwner(schain_name, address)
-
+    
+    @transaction_method
     def deposit_erc721_direct(self, schain_name: str, address: int, token_id: int, receiver: int) -> TxRes:
         return self.contract.functions.depositERC721Direct(schain_name, address, token_id, receiver)
 
