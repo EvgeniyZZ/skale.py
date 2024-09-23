bdb280650bb601aae5e7d551c5d40c288b79e23c
EvgeniyZh <evgeniy@skalelabs.com>
2024-09-23 12:56:22 +0100

    remove unsused imports

--- skale/contracts/ima/mainnet/community_pool.py
+++ skale/contracts/ima/mainnet/community_pool.py
@@ -1,6 +1,7 @@
+from Crypto.Hash import keccak
+
 from skale.contracts.base_contract import BaseContract, transaction_method
 from skale.transactions.result import TxRes
-from Crypto.Hash import keccak
 
 
 class CommunityPool(BaseContract):
--- skale/contracts/ima/mainnet/deposit_box_erc721.py
+++ skale/contracts/ima/mainnet/deposit_box_erc721.py
@@ -1,20 +1,23 @@
 from skale.contracts.base_contract import BaseContract, transaction_method
 from skale.transactions.result import TxRes
-from Crypto.Hash import keccak
 
 
 class DepositBoxERC721(BaseContract):
     @transaction_method
-    def deposit_erc721(self, schain_name: str, address: int, tokenID: int) -> TxRes: 
+    def deposit_erc721(self, schain_name: str, address: int, tokenID: int) -> TxRes:
         return self.contract.functions.depositERC721(schain_name, address, tokenID)
 
     @transaction_method
     def add_erc721_token(self, schain_name: str, address: int) -> TxRes:
         return self.contract.functions.addERC721TokenByOwner(schain_name, address)
-    
+
     @transaction_method
-    def deposit_erc721_direct(self, schain_name: str, address: int, token_id: int, receiver: int) -> TxRes:
-        return self.contract.functions.depositERC721Direct(schain_name, address, token_id, receiver)
+    def deposit_erc721_direct(
+        self, schain_name: str, address: int, token_id: int, receiver: int
+    ) -> TxRes:
+        return self.contract.functions.depositERC721Direct(
+            schain_name, address, token_id, receiver
+        )
 
     def get_schain_to_erc721(self, schain_name, token_address) -> int:
         return self.contract.functions.getSchainToERC721(schain_name, token_address)
