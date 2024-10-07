87df96530af4a0b51100cc6c116b59e02af94399
EvgeniyZh <evgeniy@skalelabs.com>
2024-10-07 15:50:22 +0100

    fix flake8 test paymaster

--- skale/contracts/paymaster/paymaster.py
+++ skale/contracts/paymaster/paymaster.py
@@ -11,25 +11,25 @@ class Paymaster(BaseContract):
         keccak_hash = keccak.new(data=schain_name.encode("utf8"), digest_bits=256)
         schain_id = keccak_hash.digest()
         return self.contract.functions.addSchain(schain_id)
-    
+
     @transaction_method
     def remove_schain(self, schain_name: str) -> TxRes:
         keccak_hash = keccak.new(data=schain_name.encode("utf8"), digest_bits=256)
         schain_id = keccak_hash.digest()
         return self.contract.functions.removeSchain(schain_id)
-    
+
     @transaction_method
     def add_validator(self, validator_id: int, validator_address: int) -> TxRes:
         return self.contract.functions.addValidator(validator_id, validator_address)
-    
+
     @transaction_method
     def remove_validator(self, validator_id: int) -> TxRes:
         return self.contract.functions.removeValidator(validator_id)
-    
+
     @transaction_method
     def set_validator_address(self, validator_id: int, new_address: int) -> TxRes:
         return self.contract.functions.setValidatorAddress(validator_id, new_address)
-    
+
     @transaction_method
     def set_active_nodes(self, validator_id: int, nodes_amount: int) -> TxRes:
         return self.contract.functions.setActiveNodes(validator_id, nodes_amount)
@@ -37,27 +37,27 @@ class Paymaster(BaseContract):
     @transaction_method
     def set_max_replenishment_period(self, month: int) -> TxRes:
         return self.contract.functions.setMaxReplenishmentPeriod(month)
-    
+
     @transaction_method
     def set_schain_price(self, price: int) -> TxRes:
         return self.contract.functions.setSchainPrice(price)
-    
+
     @transaction_method
     def set_skl_price(self, price: int) -> TxRes:
         return self.contract.functions.setSklPrice(price)
-    
+
     @transaction_method
     def set_allowed_skl_price_lag(self, lag_seconds: int) -> TxRes:
         return self.contract.functions.setAllowedSklPriceLag(lag_seconds)
-    
+
     @transaction_method
     def set_skale_token(self, token_address: int) -> TxRes:
         return self.contract.functions.setSkaleToken(token_address)
-    
+
     @transaction_method
     def clear_history(self, timestamp_before: int) -> TxRes:
         return self.contract.functions.clearHistory(timestamp_before)
-    
+
     @transaction_method
     def pay(self, schain_name: str, month: int) -> TxRes:
         keccak_hash = keccak.new(data=schain_name.encode("utf8"), digest_bits=256)
@@ -67,37 +67,43 @@ class Paymaster(BaseContract):
     @transaction_method
     def claim(self, to_address: int) -> TxRes:
         return self.contract.functions.claim(to_address)
-    
+
     @transaction_method
     def set_version(self, new_version) -> TxRes:
         return self.contract.functions.setVersion(new_version)
-    
+
     def get_schain_expiration_timestamp(self, schain_name: str) -> TxRes:
         keccak_hash = keccak.new(data=schain_name.encode("utf8"), digest_bits=256)
         schain_id = keccak_hash.digest()
         return self.contract.functions.getSchainExpirationTimestamp(schain_id).call()
-    
+
     def get_reward_amount(self, validator_id: int) -> int:
         return self.contract.functions.getRewardAmount(validator_id).call()
-    
+
     def get_nodes_number(self, validator_id: int) -> int:
         return self.contract.functions.getNodesNumber(validator_id).call()
 
-    def get_historical_active_nodes_number(self, validator_id: int, time_when: int) -> int:
-        return self.contract.functions.getHistoricalActiveNodesNumber(validator_id, time_when).call()
-    
+    def get_historical_active_nodes_number(
+        self, validator_id: int, time_when: int
+    ) -> int:
+        return self.contract.functions.getHistoricalActiveNodesNumber(
+            validator_id, time_when
+        ).call()
+
     def get_historical_total_active_nodes_number(self, time_when: int) -> int:
-        return self.contract.functions.getHistoricalTotalActiveNodesNumber(time_when).call()
+        return self.contract.functions.getHistoricalTotalActiveNodesNumber(
+            time_when
+        ).call()
 
     def get_validator_number(self, number: int) -> int:
         return self.contract.functions.getValidatorsNumber(number).call()
-    
+
     def get_schain_names(self, names_array: str) -> str:
         return self.contract.functions.getSchainsNames(names_array).call()
-    
+
     def get_total_reward(self, time_from: int, time_to: int) -> int:
         return self.contract.functions.getTotalReward(time_from, time_to).call()
-    
+
     @transaction_method
     def set_nodes_amount(self, validator_id: int, amount: int) -> TxRes:
         return self.contract.functions.setNodesAmount(validator_id, amount)
@@ -105,13 +111,6 @@ class Paymaster(BaseContract):
     @transaction_method
     def claim_for(self, validator_id: int, address: int) -> TxRes:
         return self.contract.functions.claimFor(validator_id, address)
-    
+
     def get_schain_number(self, number: int) -> str:
         return self.contract.functions.getSchainsNumber(number).call()
-
-
-    
-
-
-
-    
